4deabf77c7487116d5e78b13577d1b253050b0b0
Giovanni Fasano <46658414+Gvf63@users.noreply.github.com>
2026-09-06 19:03:51 +0200

    fix translation

--- admin/help.php
+++ admin/help.php
@@ -5,6 +5,17 @@ defined('FACETAGWRITE_PATH') or die('Hacking attempt!');
 // Afficher le tabsheet
 facetageditor_admin_tabsheet('help');
 
+$language = isset($user['language']) ? $user['language'] : 'en_UK';
+
+load_language(
+  'plugin.lang',
+  FACETAGWRITE_PATH,
+  array(
+    'language' => $language,
+    'no_fallback' => true
+  )
+);
+
 // Charger le template
 $template->set_filename('face_tag_editor_help', dirname(__FILE__).'/../template/help.tpl');
 $template->assign_var_from_handle('ADMIN_CONTENT', 'face_tag_editor_help');
